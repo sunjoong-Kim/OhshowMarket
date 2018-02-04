d24d20b6f32636ee1a6984ef970974ad8f3d4b12
Kim <Kim@DESKTOP-0H0O1Q8>
2018-02-04 21:52:06 +0900

    DDL언어 추가

---
+++ src/main/java/com/okky/test/domain/TestVO.java
@@ -0,0 +1,15 @@
+package com.okky.test.domain;
+
+public class TestVO {
+	
+	private String tt;
+
+	public String getTt() {
+		return tt;
+	}
+
+	public void setTt(String tt) {
+		this.tt = tt;
+	}
+	
+}
